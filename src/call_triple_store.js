92121e08f56c9aa59e4b9163c0721467b1fb3dbe
Gregory Saumier-Finch <gregory@culturecreates.com>
2019-10-23 13:31:41 -0400

    added call_triple_store.js

---
+++ src/call_triple_store.js
@@ -0,0 +1,27 @@
+const callTripleStore = (sparql, operation) => {
+  // operation can be "query" or "update".  If empty it will default to "update"
+
+  let url = 'http://db.artsdata.ca/repositories/artsdata';
+
+  const options = {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      Authorization: `Basic ${Utilities.base64Encode('artsdata-api:Syjcix-zovwes-7vobmi')}`
+    },
+    payload: {}
+  };
+  if (operation === 'query') {
+    options.payload = { query: sparql };
+  } else {
+    options.payload = { update: sparql };
+    url += '/statements';
+  }
+
+  const response = UrlFetchApp.fetch(url, options);
+  // Logger.log(response.getContentText());
+  return response.getContentText();
+};
+
+export default callTripleStore;
--- src/pushData.js
+++ src/pushData.js
@@ -1,5 +1,11 @@
+import callTripleStore from './call_triple_store';
+
 const pushData = data => {
-  return `Test updated ${escape(data.graph)}`;
+  const sparql =
+    ' PREFIX schema: <http://schema.org/> insert data { GRAPH <http://test.plugin.com> {<http://test.one> a schema:Organization . } }';
+
+  const result = callTripleStore(sparql);
+  return `Test updated ${data} ${JSON.stringify(result)}`;
 };
 
 export default pushData;
